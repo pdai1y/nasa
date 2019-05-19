ec4f95070ca5fe16a52dac64ef1d0f7b46569b30
pdai1y <pat.irwin4@gmail.com>
2019-05-18 22:48:13 -0400

    Clean up Rest module & update docs

--- lib/nasa/api/rest.rb
+++ lib/nasa/api/rest.rb
@@ -1,40 +1,54 @@
 # frozen_string_literal: true
+require 'nasa/api/responses'
 
 module NASA
   # CRUD Rest actions to be plug and play into all API Services
   module Rest
-    def get(url, payload: nil, params: nil)
-      request(:get, url, payload: payload, params: params)
+    extend NASA::Responses
+
+    # @!macro crud
+    #   @param [String] url the url to send a request to
+    #   @param [Hash] payload the JSON payload to send
+    #   @param [Hash] params the URL params to send
+    #   @return [NASA::RestResponse] if the API call was sucessful
+    #   @return [NASA::ExceptionResponse] if the API call failed
+
+    # @!method get(url, payload: {}, params: {})
+    #   @!macro crud
+    # @!method post(url, payload: {}, params: {})
+    #   @!macro crud
+    # @!method put(url, payload: {}, params: {})
+    #   @!macro crud
+    # @!method delete(url, payload: {}, params: {})
+    #   @!macro crud
+    %w[get post put delete].each do |action|
+      define_method(:"#{action}") do |url, payload: {}, params: {}|
+        request(:"#{action}", url, payload, params)
+      end
     end
 
-    def post(url, payload: nil, params: nil)
-      request(:post, url, payload: payload, params: params)
-    end
-
-    def put(url, payload: nil, params: nil)
-      request(:put, url, payload: payload, params: params)
-    end
-
-    def delete(url, payload: nil, params: nil)
-      request(:delete, url, payload: payload, params: params)
-    end
-
-    def request(action, url, payload: nil, params: nil)
-      options = rest_options(action, url, payload: payload, params: params)
+    # Performs a CRUD action to the requested URL
+    # @!macro crud
+    def request(action, url, payload, params)
+      options = rest_options(action, url, payload, params)
       response = RestClient::Request.execute(options)
 
-      NASA::RestResponse.new(response)
+      Responses::RestResponse.new(response)
     rescue RestClient::ExceptionWithResponse => ex
-      NASA::ExceptionResponse.new(ex)
+      Responses::ExceptionResponse.new(ex)
     end
 
     private
 
-    def headers(params)
-      { params: params }.merge!(api_key: NASA.api_key)
+    def headers(params = {})
+      {
+        params: {
+          api_key: NASA.api_key
+        }
+      }.merge!(params)
     end
 
-    def rest_options(action, url, payload: nil, params: nil)
+    def rest_options(action, url, payload, params)
       logger = Logger.new(STDERR) if NASA.debug
       rest_options = {
         method: action,
